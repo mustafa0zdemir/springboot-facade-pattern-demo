71feb8e5fbc988d46924ef9fd851eb012e9159e6
mustafa0zdemir <105823315+mustafa0zdemir@users.noreply.github.com>
2026-03-08 18:50:31 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/mustafaozdemir/facadepatterndemo/FacadePatternDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mustafaozdemir.facadepatterndemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacadePatternDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacadePatternDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustafaozdemir/facadepatterndemo/FacadePatternDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafaozdemir.facadepatterndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacadePatternDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
